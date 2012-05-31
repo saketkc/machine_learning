9d9439f843fda116c0947870294d19c7fac99aa5
Saket Choudhary <saketkc@gmail.com>
2012-05-31 14:29:43 +0100

    commit

--- k_means_learning.py
+++ k_means_learning.py
@@ -1,50 +1,45 @@
 import numpy as np
 import arff
 from sklearn import cluster
+import pylab as pl
+from collections import defaultdict
 filepath = "bondchanges.arff"
 arff_file = arff.load(filepath)
 first_entry = arff_file.next()
-#length_of_row = len(first_entry)-1
 data_array = np.array([first_entry._values[:-1]])
 target_array = np.array([first_entry._values[-1]])
-#unique_target_list = {first_entry._values[-1][0]:0}
 target_list = [first_entry._values[-1][0]]
-i = 0
 for row in arff_file:
 
     data_values = row._values[:-1]
     target_values = row._values[-1]
     target_list.append(target_values)
-    #if target_values not in unique_target_list:
-        #unique_target_list[target_values] = i
-	
-    i+=1
-    #print target_values
     data_array = np.append(data_array,[data_values],0)
     target_array = np.append(target_array,[target_values],0)
 
 arff_file.close()
-#print data_array
-#print target_array
-#print "STARTING CLUSTERING1"
 l = len(target_list)-1
-k_means = cluster.KMeans(k=l/10)
-#print "STARTING CLUSTERING2"
+k_means = cluster.KMeans(k=10)
 k_means.fit(data_array)
-#a= []
-#for value in unique_target_list.itervalues():
- #   a.append(value)
-#print "STARTING CLUSTERING"
-
-#print len(a)
-
-#for key,value in unique_target_list.iteritems():
- #   print key, "                                                   => ", k_means.labels_[value] 
-#print len(target_list)
-#print len(k_means.labels_)
+k_means_labels = k_means.labels_
+k_means_cluster_centers = k_means.cluster_centers_
+first_ec_number_map = defaultdict(list)
 while (l>=0):
-   print target_list[l], "                       =>",  k_means.labels_[l]
+   first_ec_number = target_list[l].split('.')[0]
+   first_ec_number_map[first_ec_number].append(k_means.labels_[l])
+	
    l = l-1
 
-
-
+#print  first_ec_number_map
+colors = ['#FF0000','#00FF00','#0000FF','#FFFF00','#00FFFF','#000000']
+#fig = pl.figure(figsize=(8, 3))
+#ax = fig.add_subplot(1, 3, 1)
+#for k, col in zip(range(6), colors):
+ #   my_members = k_means_labels == k
+    #print my_members
+  #  cluster_center = k_means_cluster_centers[k]
+  #  ax.plot(X[my_members, 0], X[my_members, 1], 'w',  markerfacecolor=col, marker='.')
+#pl.scatter(data_array[:, 0], data_array[:, 1])
+#pl.scatter(k_means.cluster_centers_[:, 0], k_means.cluster_centers_[:, 1], marker='o', s=100)
+#pl.show()
+print data_array[:, 0]
--- k_nearest_neighbors.py
+++ k_nearest_neighbors.py
@@ -1,1 +1,46 @@
+import numpy as np
+import arff
+from sklearn import cluster
+from sklearn import svm
+from sklearn.neighbors import KNeighborsClassifier
+def arff_read_to_array(filepath):
+	
+	arff_file = arff.load(filepath) #Load File
+	first_entry = arff_file.next()  #read First Data entry 
+	data_array = np.array([first_entry._values[:-1]])
+	target_array = np.array([first_entry._values[-1]])
+	target_list = [first_entry._values[-1][0]]
+	for row in arff_file:
+	    data_values = row._values[:-1]
+	    target_values = row._values[-1]
+	    target_list.append(target_values)
+	    data_array = np.append(data_array,[data_values],0)
+	    target_array = np.append(target_array,[target_values],0)
+	arff_file.close()
+	l = len(target_list)-1
+	return {"target" : target_array,"data": data_array}
 
+def k_nearest_neighbours():
+	filepath = "bondchanges.arff"
+	all_data = arff_read_to_array(filepath)
+	X_data = all_data["data"]
+	Y_data = all_data["target"]
+	Y_data_map = {}
+        new_Y_data = np.array([])
+	for index,data in enumerate(Y_data):
+		Y_data_map[index] = data
+                new_Y_data = np.append(new_Y_data,[index],0) #Create
+	X_training = X_data[:0.1*len(X_data)]
+	Y_training = new_Y_data[:0.1*len(Y_data)]
+	print X_training
+	print 
+        print Y_training
+	X_test = X_data[0.9*len(X_data):]
+	Y_test = new_Y_data[0.9*len(Y_data):]
+	#svc = svm.SVC(C=1, kernel='')
+	knn = KNeighborsClassifier() 
+	print knn.fit(X_training, Y_training).score(X_test,Y_test)
+
+filepath = "bondchanges.arff"
+#print arff_read_to_array(filepath)
+k_nearest_neighbours()
