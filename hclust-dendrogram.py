975a9abaee00a487cc59aa5ea30cc5b7d26806ad
Saket Choudhary <saketkc@gmail.com>
2012-06-26 18:12:36 +0100

    new files for orange

--- hclust-dendrogram.py
+++ hclust-dendrogram.py
@@ -4,5 +4,5 @@ import orngClustering
 data = orange.ExampleTable("allchanges.arff")
 #sample = data.selectref(orange.MakeRandomIndices2(data, 20), 0)
 root = orngClustering.hierarchicalClustering(data)
-orngClustering.dendrogram_draw("allchanges.png", root,labels=[str(d.getclass()) for d in data]) 
+#orngClustering.dendrogram_draw("allchanges.png", root,labels=[str(d.getclass()) for d in data]) 
 
